94075867f479bb343137ef24e81c1724ed621868
vsaran93 <vishnusaran37@gmail.com>
2022-05-14 21:12:30 +0530

    initial commit

---
+++ EmployeeManagement/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EmployeeManagement.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+
+        public List<Department> Departments { get; set; }
+    }
+}
